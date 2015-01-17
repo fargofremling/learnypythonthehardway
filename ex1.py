15700fd55d2a5eec82d8c7cf7aea57fa9f176218
fargofremling <alicia.fremling@gmail.com>
2015-01-17 09:59:17 -0800

    Add Exercise 1 File

---
+++ ex1.py
@@ -0,0 +1,32 @@
+#Chapter 1 Exercise
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+#Chapter 1 Study Drills
+
+#1. Make your script print another line.
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "This is my extra line"
+
+#2. Make your script print only one of the lines.
+print "This is fun."
+
+#3. Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does
+print "Hello World!"
+#print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
