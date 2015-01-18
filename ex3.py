0b2e91afd165ccb5ee0b6d6e27593fa2c442c3a6
fargofremling <alicia.fremling@gmail.com>
2015-01-18 09:03:38 -0800

    Add Exercise 3 File

---
+++ ex3.py
@@ -0,0 +1,102 @@
+# Chapter 3 Exercise
+
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+
+# Chapter 3 Study Drills
+
+
+# 1. Above each line, use the "#" to write a comment to yourself explaining what the line does.
+
+#Prints the statement to count chickens
+print "I will now count my chickens:"
+
+#Calculates and prints the sum of Hens
+print "Hens", 25 + 30 / 6
+#Calculates and prints the sum of Roosters
+print "Roosters", 100 - 25 * 3 % 4
+
+#Prints the statement to count eggs
+print "Now I will count the eggs:"
+
+#Calculates the sum of eggs
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#Asks an inequality question
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#Analyzes the inequality and returns a Boolean value
+print 3 + 2 < 5 - 7
+
+#Asks an arithmetic problem and calculates the sum
+print "What is 3 + 2?", 3 + 2
+#Asks an arithmetic problem and calculates the sum
+print "What is 5 - 7?", 5 - 7
+
+#Prints the statement that the solution is false
+print "Oh, that's why it's False."
+
+#Requests more arithmetic problems
+print "How about some more."
+
+#Asks and analyzes an inequality
+print "Is it greater?", 5 > -2
+#Asks and analyzes an inequality
+print "Is it greater or equal?", 5 >= -2
+#Asks and analyzes an inequality
+print "Is it less or equal?", 5 <= -2
+
+
+#2. Find something that needs to be calculated and write code that does it.
+
+print "How much older is my mom than me?"
+print "My mom is 65 years old, and I am 31 years old."
+print "She is", 65 - 31, "years older."
+
+#3. Rewrite ex3.py to use floating point numbers so it's more accurate. 20.0 is floating point.
+
+print "I will now count my chickens:"
+
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3.0 + 2.0 < 5.0 - 7.0
+
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
