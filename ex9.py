cab9649a459d75eafdfbe42f78154a6583cdef7c
fargofremling <alicia.fremling@gmail.com>
2015-01-24 23:46:33 -0800

    Add Exercise 9 File

---
+++ ex9.py
@@ -0,0 +1,23 @@
+# Chapter 9 Exercise
+
+# Here's some new strange stuff. Remember, type it exactly as it's printed.
+
+days = " Mon Tue Wed Thu Fri Sat Sun"
+months =  "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "here are the months: ", months
+
+print """
+    
+    There's something going on here.
+    With the three double-quotes.
+    We'll be able to type as much as we like. 
+    Even 4 lines if we want, or 5, or 6. 
+    
+    """
+
+# Chapter 9 Study Drills
+
+# 1. Check your work, write down your mistakes, try not to make them on the next exercise.
+# None. End of questions. 
