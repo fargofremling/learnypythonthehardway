5a46d737fd041be500eae9e76f04aa160ee026d8
fargofremling <alicia.fremling@gmail.com>
2015-02-10 23:04:56 -0800

    Update Exercise 20 File

--- ex20/ex20.py
+++ ex20/ex20.py
@@ -51,3 +51,13 @@ print_a_line(current_line, current_file)
 
 # 1. Write English comments for each line to understand what that line does.
 #
+
+
+
+# 2. Each time print_a_line is run, you are passing in a variable current_line. Write out what current_line is equal to on each function call, and trace how it becomes line_count in print_a_line.
+# 3. Find each place a function is used, and check its def to make sure that you are giving it the right arguments.
+# 4. Research online what the seek function for file does. Try pydoc file and see if you can figure it out from there.
+# A seek() operation moves that pointer to some other part of the file so it can be read or written to at that place.
+
+# 5. Research the shorthand notation += and rewrite the script to use += instead.
+# The += creates an augmented assignement statement.  Basically it would be a simplified version of the code rewritten on lines 43 and 46. It is important to note that the when this occurs, rather than creating a new object and assigning that to the target, the old object is modified instead.
