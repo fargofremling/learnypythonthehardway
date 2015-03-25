d02dc1774dff4ee1f8f33d7f5c72f65c4fe49b33
fargofremling <alicia.fremling@gmail.com>
2015-03-24 22:31:07 -0700

    Add study drills for Exercise 25

--- ex25.py
+++ ex25.py
@@ -33,3 +33,9 @@ def print_first_and_last_sorted(sentence):
     words = sort_sentence(sentence)
     print_first_word(words)
     print_last_word(words)
+
+# Study Drills
+# 1. Take the remaining lines of the What You Should See output and figure out what they are doing. Make sure you understand how you are running your functions in the ex25 module.
+# 2. Try doing this: help(ex25) and also help(ex25.break_words). Notice how you get help for your module, and how the help is those odd """ strings you put after each function in ex25? Those special strings are called documentation comments and we'll be seeing more of them.
+# 3. Typing ex25. is annoying. A shortcut is to do your import like this: from ex25 import * which is like saying, "Import everything from ex25." Programmers like saying things backward. Start a new session and see how all your functions are right there.
+# 4. Try breaking your file and see what it looks like in python when you use it. You will have to quit python with quit() to be able to reload it.
