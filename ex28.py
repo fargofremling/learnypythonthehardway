adc80c141e85cf283677b399057d6ca98a817d00
fargofremling <alicia.fremling@gmail.com>
2016-06-20 22:41:35 -0500

    Add Exercise 28 File

---
+++ ex28.py
@@ -0,0 +1,34 @@
+True and False # False
+False and True # False
+1 == 1 and 2 == 1 # False
+"test" == "test" # True
+1 == 1 or 2 != 1 # True
+True and 1 == 1 # True
+False and 0 != 0 # False
+True or 1 == 1 # True
+"test" == "testing" # False
+1 != 0 and 2 == 1 # False
+"test" != "testing" # True
+"test" == 1 # False
+not (True and False) # True
+not (1 == 1 and 0 != 1) # False
+not (10 == 1 or 1000 == 1000) # False
+not (1 != 10 or 3 == 4) # False
+not ("testing" == "testing" and "Zed" == "Cool Guy") # True
+1 == 1 and (not ("testing" == 1 or 1 == 0)) # True
+"chunky" == "bacon" and (not (3 == 4 or 3 == 3)) # False
+3 == 3 and (not ("testing" == "testing" or "Python" == "fun")) # False
+
+#100% correct on the first go!
+
+# Study Drills
+# 1. There are a lot of operators in Python similar to != and ==. Try to find as many "equality operators" as you can. 
+#	They should be like < or <=.
+# 2. Write out the names of each of these equality operators. For example, I call != "not equal."
+
+#  == equal
+#  != not equal
+#  > greater than
+#  >= greater than or equal to
+#  < less than
+#  <= less than or equal to
