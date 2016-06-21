f51efb69189af8266a6f4e2150aa6f3a70085356
fargofremling <alicia.fremling@gmail.com>
2016-06-20 21:14:25 -0700

    Add Exercise 29 File

---
+++ ex29.py
@@ -0,0 +1,40 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+	print "Too many cats! The world is doomed!"
+
+if people > cats: 
+	print "Not too many cats! The world is saved!"
+
+if people < dogs:
+	print "The world is drooled on!"
+
+if people > dogs:
+	print "The world is dry!"
+
+
+dogs += 5
+
+if people >= dogs:
+	print "People are greater than or equal to dogs."
+
+if people <= dogs:
+	print "People are less than or equal to dogs."
+
+
+if people == dogs:
+	print "People are dogs."
+
+
+# Study Drills
+# 1. What do you think the if does to the code under it?
+
+# 2. Why does the code under the if need to be indented four spaces?
+
+# 3. What happens if it isn't indented?
+# - There will be a syntax error and the code will not run.
+# 4. Can you put other boolean expressions from Exercise 27 in the if-statement? Try it.
+# 5. What happens if you change the initial values for people, cats, and dogs?
