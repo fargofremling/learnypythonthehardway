2c28989d507b0ddd40feee4878ae2a03375f5e3f
fargofremling <alicia.fremling@gmail.com>
2016-06-25 23:05:23 -0700

    fix initial code and start Study Drill

--- ex31.py
+++ ex31.py
@@ -1,11 +1,11 @@
-print "You enter a dark room with two doors.  Do you go through door #1 or door #2?"
+print "You enter a dark room with two doors. Do you go through door #1 or door #2?"
 
-raw_input("> ")
+door = raw_input("> ")
 
 if door == "1":
     print "There's a giant bear here eating a cheese case. What eating a cheese cake.  What do you do?"
     print "1. Take the cake."
-    print "2. Screan at the bear."
+    print "2. Scream at the bear."
     
     bear = raw_input("> ")
     
@@ -25,12 +25,54 @@ elif door == "2":
     insanity = raw_input("> ")
     
     if insanity == "1" or insanity == "2":
-        print "Your body survives powered by a mind of jello. Good job!
+        print "Your body survives powered by a mind of jello.Good job!"
+    else: 
+        print "The insanitiy rots your eyes into a pool of muck. Good job!"
+
+else:
+    print "You stumble around and fall on a knife and die. Good job!"
+
+# Study Drill
+# Write a completely new game. Maybe you don't like this one, so make your own. 
+# This is your computer, do what you want.
+
+print "Choose your own adventure #2"
+
+print "You are on the Price is Right."
+print "The host gives you the option to chose among three briefcases."
+
+suitcase = raw_input("Chose 1, 2 or 3 > ")
+
+if suitcase == "1":
+    print """There's $1,000 cash, and a note that says, 
+    \"You can keep the cash, or give it up and chose a different briefcase.
+    One suitcase has a value greater than $1,000, the other has a lesser value.\""""
+    print "1. Keep the $1,000 cash"
+    print "2. Give up the $1,000 and pick briefcase #2"
+    print "3. Give up the $1,000 and pick briefcase #3"
+    
+    cash_choice = raw_input("> ")
+    
+    if cash_choice == "1":
+        print "Nice, $1,000 is a good chunk of change!"
+    elif cash_choice == "2":
+        print "Let's see what's in suitcase two...$5,000!"
+    else:
+        print """Well, maybe you can go to dinner with tht $50?
+        Better luck next time."""
+
+elif suitcase == "2":
+    print "You stare into the endless abyss at Cthulhu's retina."
+    print "1. Blueberries."
+    print "2. Yellow jacket clothespins."
+    print "3. Understanding revolvers yelling melodies."
+    
+    insanity = raw_input("> ")
+    
+    if insanity == "1" or insanity == "2":
+        print "Your body survives powered by a mind of jello. Good job!"
     else: 
         print "The insanitiy rots your eyes into a pool of muck.  Good job!"
 
 else:
     print "You stumble around and fall on a knife and die. Good job!"
-
- 
-    
