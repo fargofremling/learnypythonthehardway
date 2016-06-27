adf524cc985fb596f48bda0b593b09a56e43048c
fargofremling <alicia.fremling@gmail.com>
2016-06-26 23:26:55 -0700

    Finish Study Drill for Ch. 31.

--- ex31.py
+++ ex31.py
@@ -35,44 +35,66 @@ else:
 # Study Drill
 # Write a completely new game. Maybe you don't like this one, so make your own. 
 # This is your computer, do what you want.
+legal_choice = False
 
-print "Choose your own adventure #2"
-
-print "You are on the Price is Right."
-print "The host gives you the option to chose among three briefcases."
-
-suitcase = raw_input("Chose 1, 2 or 3 > ")
-
-if suitcase == "1":
-    print """There's $1,000 cash, and a note that says, 
-    \"You can keep the cash, or give it up and chose a different briefcase.
-    One suitcase has a value greater than $1,000, the other has a lesser value.\""""
-    print "1. Keep the $1,000 cash"
-    print "2. Give up the $1,000 and pick briefcase #2"
-    print "3. Give up the $1,000 and pick briefcase #3"
-    
-    cash_choice = raw_input("> ")
-    
+def briefcase_options():
     if cash_choice == "1":
         print "Nice, $1,000 is a good chunk of change!"
     elif cash_choice == "2":
-        print "Let's see what's in suitcase two...$5,000!"
-    else:
-        print """Well, maybe you can go to dinner with tht $50?
-        Better luck next time."""
+        print "Let's see what's in suitcase 2...$5,000! Good choice!"
+    elif cash_choice == "3":
+        print "With suitcase 3, you can ...well, buy dinner with that $50? Better luck next time."
+            
+print "Choose your own adventure #2"
+
+print "You are on the Price is Right."
+print "The host gives you the option to choose among three briefcases."
+
+suitcase = raw_input("Choose 1, 2 or 3 > ")
+
+if suitcase == "1":
+    print """There's $1,000 cash, and a note that says, 
+    \"You can keep the cash, or give it up and choose a different briefcase.
+    One suitcase has a value greater than $1,000, the other has a lesser value.\""""
+    print "1. Keep the $1,000 cash."
+    print "2. Give up the $1,000 and pick briefcase #2."
+    print "3. Pick briefcase #3 and give up the $1,000."
+    
+    cash_choice = raw_input("> ")
+    
+    while legal_choice is False:
+        if cash_choice in ["1", "2", "3"]:
+#       Why does this list have to be strings? Does raw_input only accept strings?
+            briefcase_options()
+            break
+#           legal_choice = True ^^ which is a better way to break the loop?
+        else:
+            print "That's not an option. Please choose 1, 2 or 3."
+            cash_choice = raw_input("> ")
+            briefcase_options()
+            continue
 
 elif suitcase == "2":
-    print "You stare into the endless abyss at Cthulhu's retina."
-    print "1. Blueberries."
-    print "2. Yellow jacket clothespins."
-    print "3. Understanding revolvers yelling melodies."
-    
-    insanity = raw_input("> ")
-    
-    if insanity == "1" or insanity == "2":
-        print "Your body survives powered by a mind of jello. Good job!"
-    else: 
-        print "The insanitiy rots your eyes into a pool of muck.  Good job!"
+    print "It's your lucky day! You picked the largest prize of $5,0000!"
 
+elif suitcase == "3": 
+    print ("You picked the smallest prize possible, a mere $50. "
+    "However, since this is the Price is Right, "
+    "and we want our audience members to go home as big winners, "
+    "choose another suitcase.")
+    
+    print "Choose between briefcase 1 or 2."
+   
+    better_choice = raw_input("> ")
+    
+    if better_choice == "1":
+        print "Much better...$1,000!"
+
+    elif better_choice == "2":
+        print "You just increased your winning by 100 times. You won $5,000!"
+        
+    else: 
+        print "Game over, Chump. You didn't choose between 1 oe 2."
+        
 else:
-    print "You stumble around and fall on a knife and die. Good job!"
+    print "Game over. You didn't choose among the three suitcases."
