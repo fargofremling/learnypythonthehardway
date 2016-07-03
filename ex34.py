65097c625d0b60830a5174b9a10893d1a42c0265
fargofremling <alicia.fremling@gmail.com>
2016-07-02 22:17:15 -0700

    Exercise 34 File with Study Drills

---
+++ ex34.py
@@ -0,0 +1,68 @@
+# Accessing Elements of a List
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print animals
+
+# 1. The animal at 1: python
+# 2. The thirds (3rd) animal: peacock
+# 3. The first (1st) animal: bear
+# 4. The animal at 3: kangaroo
+# 5. The fifth (5th) animal: whale
+# 6. The animal at 2: peacock
+# 7. The sixth (6th) animal: platypus
+# 8. The animal at 4: whale
+
+print "The first (1st) animal is at 0 and is a bear."
+print animals[0]
+
+print "The second (2nd) animal is at 1 and is a python."
+print animals[1]
+
+print "The third (3rd) animal is at 2 and is a peacock."
+print animals[2]
+
+print "The fourth (4th) animal is at 3 and is a kangaroo."
+print animals[3]
+
+print "The fifth (5th) animal is at 4 and is a whale." 
+print animals[4]
+
+print "The sixth (6th) animal is at 5 and is a platypus."
+print animals[5]
+
+# Study Drills
+
+# 1. With what you know of the difference between these types of numbers, can you explain 
+# why the year 2010 in "January 1, 2010," really is 2010 and not 2009? (Hint: you can't 
+# pick years at random.)
+# - January 1, 2010 is in fact 2010, because years are ordinal numbers, therefore 1 is 1, 
+# - and 2010 is 2010.
+# 2. Write some more lists and work out similar indexes until you can translate them.
+# 3. Use Python to check your answers.
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
+
+print colors
+
+print "The first (1st) color is at 0 and is a red."
+print colors[0]
+
+print "The second (2nd) color is at 1 and is a orange."
+print colors[1]
+
+print "The third (3rd) color is at 2 and is a yellow."
+print colors[2]
+
+print "The fourth (4th) color is at 3 and is a green."
+print colors[3]
+
+print "The fifth (5th) color is at 4 and is a blue." 
+print colors[4]
+
+print "The sixth (6th) color is at 5 and is a indigo."
+print colors[5]
+
+print "The seventh (7th) color is at 6 and is a purple."
+print colors[6]
+
+
